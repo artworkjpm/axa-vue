998517d79330d9c27fbb0a7e21781f46dcb23232
JohnMoran <johnnymoran44@hotmail.com>
2021-04-22 14:58:47 +0200

    Update TopNavBar.vue

--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -1,79 +1,28 @@
 <template>
-	<div class="page-container md-layout-column">
+	<div class="md-layout-column">
 		<md-toolbar class="md-primary">
 			<md-button class="md-icon-button" @click="showNavigation = true">
 				<md-icon>menu</md-icon>
 			</md-button>
-			<span class="md-title">My Title</span>
-
-			<div class="md-toolbar-section-end">
-				<md-button @click="showSidepanel = true">Favorites</md-button>
-			</div>
+			<span class="md-title">Brastlewark Population</span>
 		</md-toolbar>
 
 		<md-drawer :md-active.sync="showNavigation" md-swipeable>
 			<md-toolbar class="md-transparent" md-elevation="0">
-				<span class="md-title">My App name</span>
+				<span class="md-title">Brastlewark Population</span>
 			</md-toolbar>
 
 			<md-list>
-				<md-list-item>
-					<md-icon>move_to_inbox</md-icon>
-					<span class="md-list-item-text">Inbox</span>
-				</md-list-item>
-
 				<md-list-item>
 					<md-icon>send</md-icon>
-					<span class="md-list-item-text">Sent Mail</span>
+					<span class="md-list-item-text">About this app</span>
 				</md-list-item>
-
 				<md-list-item>
-					<md-icon>delete</md-icon>
-					<span class="md-list-item-text">Trash</span>
-				</md-list-item>
-
-				<md-list-item>
-					<md-icon>error</md-icon>
-					<span class="md-list-item-text">Spam</span>
+					<md-icon>send</md-icon>
+					<span class="md-list-item-text">About John Moran</span>
 				</md-list-item>
 			</md-list>
 		</md-drawer>
-
-		<md-drawer class="md-right" :md-active.sync="showSidepanel">
-			<md-toolbar class="md-transparent" md-elevation="0">
-				<span class="md-title">Favorites</span>
-			</md-toolbar>
-
-			<md-list>
-				<md-list-item>
-					<span class="md-list-item-text">Abbey Christansen</span>
-
-					<md-button class="md-icon-button md-list-action">
-						<md-icon class="md-primary">chat_bubble</md-icon>
-					</md-button>
-				</md-list-item>
-
-				<md-list-item>
-					<span class="md-list-item-text">Alex Nelson</span>
-
-					<md-button class="md-icon-button md-list-action">
-						<md-icon class="md-primary">chat_bubble</md-icon>
-					</md-button>
-				</md-list-item>
-
-				<md-list-item>
-					<span class="md-list-item-text">Mary Johnson</span>
-
-					<md-button class="md-icon-button md-list-action">
-						<md-icon>chat_bubble</md-icon>
-					</md-button>
-				</md-list-item>
-			</md-list>
-		</md-drawer>
-
-		<md-content>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.
-		</md-content>
 	</div>
 </template>
 
@@ -87,21 +36,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.page-container {
-	min-height: 300px;
-	overflow: hidden;
-	position: relative;
-	border: 1px solid rgba(#000, 0.12);
-}
-
-// Demo purposes only
-.md-drawer {
-	width: 230px;
-	max-width: calc(100vw - 125px);
-}
-
-.md-content {
-	padding: 16px;
-}
-</style>
+<style lang="scss" scoped></style>
