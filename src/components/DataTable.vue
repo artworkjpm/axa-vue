06034bc389d8bdaa803477b16031f0fb76ae7f48
JohnMoran <johnnymoran44@hotmail.com>
2021-04-23 10:23:12 +0200

    Update DataTable.vue

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,10 +1,15 @@
 <template>
-	<div class="hello">
-		<h1>{{ msg }}</h1>
-		<md-table v-model="posts" md-sort="name" md-sort-order="asc" md-card md-fixed-header>
+	<div>
+		<md-table :value="posts" md-sort="name" md-sort-order="asc" md-card md-fixed-header>
 			<md-table-toolbar>
-				<h1 class="md-title">Users</h1>
+				<md-field md-clearable class="md-toolbar-section-end">
+					<md-input placeholder="Search by name..." v-model="search" @input="searchOnTable" />
+				</md-field>
 			</md-table-toolbar>
+
+			<md-table-empty-state md-label="No users found" :md-description="`No user found for this '${search}' query. Try a different search term or create a new user.`">
+				<md-button class="md-primary md-raised" @click="newUser">Create New Character</md-button>
+			</md-table-empty-state>
 
 			<md-table-row slot="md-table-row" slot-scope="{ item }">
 				<md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
@@ -15,13 +20,23 @@
 </template>
 
 <script>
+const toLower = (text) => {
+	return text.toString().toLowerCase();
+};
+
+const searchByName = (items, term) => {
+	if (term) {
+		return items.filter((item) => toLower(item.name).includes(toLower(term)));
+	}
+
+	return items;
+};
+
 export default {
 	name: "DataTable",
-	data() {
-		return {
-			msg: "Welcome to my Vuex Store",
-		};
-	},
+	data: () => ({
+		search: null,
+	}),
 	mounted() {
 		this.$store.dispatch("getPosts");
 	},
@@ -29,6 +44,22 @@ export default {
 		posts() {
 			return this.$store.state.posts;
 		},
+
+		searched: {
+			get: () => this.$store.state.posts,
+			set: (value) => value,
+		},
+	},
+	methods: {
+		newUser() {
+			window.alert("Noop");
+		},
+		searchOnTable() {
+			this.searched = searchByName(this.posts, this.search);
+		},
+	},
+	created() {
+		console.log("POSTS", this.posts);
 	},
 };
 </script>
